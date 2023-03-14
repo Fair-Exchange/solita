558406884d94f8e0888eb88293f4b5ff965db69e
jeffowy <jjgallow@gmail.com>
2023-03-13 20:56:35 -0600

    clear

--- test/anchor-examples/basic-2/src/index.ts
+++
@@ -1,2 +0,0 @@
-export * from './generated/accounts'
-export * from './generated/instructions'
--- test/anchor-examples/basic-3/src/index.ts
+++
@@ -1,3 +0,0 @@
-export * from './generated/puppet/accounts'
-export * from './generated/puppet/instructions'
-export * from './generated/puppet_master/instructions'
